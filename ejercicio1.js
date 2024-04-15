e136f6e08d86e5c0ce59552ca90a1fd0d62fb58c
duardoAdrianEscobarVeraADSO <eduardoadrianescobar12@gmail.com>
2024-04-14 19:45:16 -0500

    avance

---
+++ ejercicio1.js
@@ -0,0 +1,22 @@
+
+// Diseñar un programa que permita validar el tipo de tarjeta de crédito que
+// registra el operador, para ello debemos tener en cuenta que las diferentes
+// tarjetas tienen diferentes nomenclaturas:
+// • American Express: 15 dígitos y empieza por (34 – 37)
+// • Diners Club: tiene 14 dígitos y empieza por (300 – 305 o 36 - 38)
+// • Discover: tiene 16 dígitos y empieza por (6011)
+// • Mastercard: tiene 16 dígitos y empieza por (51 – 55)
+// • Visa: tiene: 16 dígitos y empieza por (4)
+
+import { validarTarjeta } from "./module.js";
+
+while (true) {
+  let numeroTarjeta = prompt("Ingrese el número de su tarjeta de crédito o 'salir' para finalizar:");
+  
+  if (numeroTarjeta.toLowerCase() === 'salir') {
+    break;
+  }
+
+  let resultado = validarTarjeta(numeroTarjeta);
+  alert(resultado);
+}
